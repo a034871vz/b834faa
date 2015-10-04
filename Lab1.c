8e13a4932ab41f7fd1cb0a757cc24f99951a40dc
a034871vz <a034871vz@gmail.com>
2015-10-04 20:19:43 +0300

    Добавил коменты.
    Добавил коменты к G,F,Y.

--- Lab1.c
+++ Lab1.c
@@ -7,21 +7,21 @@ printf("Vvedite x: ");
 scanf ("%f", &x);
 printf("Vvedite a ");
 scanf ("%f", &a);
-G=-5*(-2*a*a+a*x+3*x*x)/(10*a*a+11*a*x+3*x*x);
+G=-5*(-2*a*a+a*x+3*x*x)/(10*a*a+11*a*x+3*x*x);//Вычисление значения G
 printf("G=%f\n\n", G);
 
 printf("Vvedite x ");
 scanf ("%f", &x);
 printf("Vvedite a ");
 scanf ("%f", &a);
-F=sin(10*a*a-7*a*x+x*x);
+F=sin(10*a*a-7*a*x+x*x);//Вычисление значения F
 printf("F=%f\n\n", F);
 
 printf("Vvedite x ");
 scanf ("%f", &x);
 printf("Vvedite a ");
 scanf ("%f", &a);
-Y=atan(45*a*a-79*a*x+30*x*x);
+Y=atan(45*a*a-79*a*x+30*x*x);//Вычисление значения Y
 printf("Y=%f\n\n", Y);
 return 0;
 }
