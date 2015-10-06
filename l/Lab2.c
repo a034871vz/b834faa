f60a335326180381fc25eac64e4aae6c668cd276
a034871vz <a034871vz@gmail.com>
2015-10-06 18:54:05 +0300

    Лаба вторая.
    С коментами.

---
+++ l/Lab2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+double a,x,G,F,Y;
+int k,m;
+printf("Vvedite x: ");
+scanf ("%fl", &x);
+printf("Vvedite a ");
+scanf ("%fl", &a);
+G=-5*(-2*a*a+a*x+3*x*x)/(10*a*a+11*a*x+3*x*x);         //Вычисление значения G
+printf("G=%fl\n\n", G);
+
+printf("Vvedite x ");
+scanf ("%fl", &x);
+printf("Vvedite a ");
+scanf ("%fl", &a);
+k=10*a*a-7*a*x+x*x;                                    //Вычисление значения k
+if (k>=-1 && k<=1){F=sin(k);printf("F=%fl\n\n", F);}    //если k от -1 до 1, то вычислять и выводить F
+else{printf("Neudovletvorjaet znacheniu k\n\n");}            //если k любое другое значение, то выводить текст
+
+printf("Vvedite x ");
+scanf ("%fl", &x);
+printf("Vvedite a ");
+scanf ("%fl", &a);
+m=45*a*a-79*a*x+30*x*x;                                //Вычисление значения m
+if (m>=-1 && m<=1){Y=atan(m);printf("Y=%fl\n\n", Y);}   //если m от -1 до 1, то вычислять и выводить Y
+else{printf("Neudovletvorjaet znacheniu k\n\n");}            //если m любое другое значение, то выводить текст
+return 0;
+}
