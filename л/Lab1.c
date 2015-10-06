6d5f00901946a810150e221dd71a469751209015
a034871vz <a034871vz@gmail.com>
2015-10-06 18:55:26 +0300

    Лаба первая.
    С коментами.

---
+++ л/Lab1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+float a,x,G,F,Y;
+printf("Vvedite x: ");
+scanf ("%f", &x);
+printf("Vvedite a ");
+scanf ("%f", &a);
+G=-5*(-2*a*a+a*x+3*x*x)/(10*a*a+11*a*x+3*x*x);//Вычисление значения G
+printf("G=%f\n\n", G);
+
+printf("Vvedite x ");
+scanf ("%f", &x);
+printf("Vvedite a ");
+scanf ("%f", &a);
+F=sin(10*a*a-7*a*x+x*x);//Вычисление значения F
+printf("F=%f\n\n", F);
+
+printf("Vvedite x ");
+scanf ("%f", &x);
+printf("Vvedite a ");
+scanf ("%f", &a);
+Y=atan(45*a*a-79*a*x+30*x*x);//Вычисление значения Y
+printf("Y=%f\n\n", Y);
+return 0;
+}
